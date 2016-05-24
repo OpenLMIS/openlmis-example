546156e7ea893fbc2149c49920e962f212eaf7c1
Chongsun Ahn <cahn922@yahoo.com>
2016-05-24 10:36:17 -0700

    Add first attempt for i18n
    First attempt for i18n, with a local resolver and message resource beans. This will be replaced with something more robust, using yaml resource files and code that will allow runtime (and offline) changing of the locale.

--- src/main/java/org/openlmis/example/web/ServiceNameController.java
+++ src/main/java/org/openlmis/example/web/ServiceNameController.java
@@ -1,14 +1,25 @@
 package org.openlmis.example.web;
 
 import org.openlmis.example.util.ServiceSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class ServiceNameController {
 
+    @Autowired
+    private MessageSource messageSource;
+    
     @RequestMapping("/")
     public ServiceSignature index() {
         return new ServiceSignature(ServiceSignature.SERVICE_NAME, ServiceSignature.SERVICE_VERSION);
+    }
+
+    @RequestMapping("/hello")
+    public String test() {
+        return messageSource.getMessage("message.hello.world", null, LocaleContextHolder.getLocale());
     }
 }
