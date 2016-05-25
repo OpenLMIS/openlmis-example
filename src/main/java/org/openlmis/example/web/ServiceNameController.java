1308003acc07f3481d58392b0d45135ba7871757
Chongsun Ahn <cahn922@yahoo.com>
2016-05-25 12:13:55 -0700

    Add logging to rsyslog
    Taken from changes in the Independent Service template.

--- src/main/java/org/openlmis/example/web/ServiceNameController.java
+++ src/main/java/org/openlmis/example/web/ServiceNameController.java
@@ -1,6 +1,8 @@
 package org.openlmis.example.web;
 
 import org.openlmis.example.util.ServiceSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class ServiceNameController {
 
+    Logger logger = LoggerFactory.getLogger(ServiceNameController.class);
+
     @Autowired
     private MessageSource messageSource;
     
     @RequestMapping("/")
     public ServiceSignature index() {
+        logger.info("Returning service name and version");
         return new ServiceSignature(ServiceSignature.SERVICE_NAME, ServiceSignature.SERVICE_VERSION);
     }
 
     @RequestMapping("/hello")
     public String test() {
+        logger.info("Returning hello world message");
         return messageSource.getMessage("message.hello.world", null, LocaleContextHolder.getLocale());
     }
 }
