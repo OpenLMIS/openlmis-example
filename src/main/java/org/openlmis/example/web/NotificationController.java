2d279293d791c174a856f3be3399eb37825b4e16
Ben Leibert <ben.leibert@villagereach.org>
2016-06-03 14:56:59 -0700

    Add documentation to the new sample code.

--- src/main/java/org/openlmis/example/web/NotificationController.java
+++ src/main/java/org/openlmis/example/web/NotificationController.java
@@ -2,7 +2,11 @@ package org.openlmis.example.web;
 
 import org.openlmis.example.repository.ReadOnlyFooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,14 +25,13 @@ public class NotificationController
         members currently defined in it have no affect on our REST API.
 
         Although it works, the hybrid Spring Data Rest / Spring MVC approach represented by the getCount() method below is awkward.
-        As we've discussed, it fragments our API into an inconsistent mix of hypermedia and non-hypermedia responses. It also breaks
-        the hypermedia of the Spring Data Rest responses. Specifically, because they don't include the links exposed manually via MVC
-        ("/api/foos/count," in this case), they present incomplete and therefore misleading  "_links" objects.
-
-        Due to the above limitations, additional research into Spring Data Rest may be useful. The new findById() method added to
-        ReadOnlyFooRepository represents a very incomplete foray into what additionally might be done.
+        Without additional work to incorporate HATEOAS into MVC, the hybrid approach  fragmenting our API into an inconsistent mix of
+        hypermedia and non-hypermedia responses. It also breaks the hypermedia of the Spring Data Rest responses. Specifically, because
+        they don't include the links exposed manually via MVC ("/api/foos/count," in this case), they present incomplete and therefore
+        misleading  "_links" objects. These drawbacks, along with the fact that we're decidedly uncommitted to HATEOAS, may
+        lead us to eventually drop Spring Data Rest. In that case, we'd consistently rely on Spring MVC.
      */
-    @RequestMapping("/api/foos/count")
+    @RequestMapping(path = "/api/foos/count", method = RequestMethod.GET)
     public long getCount()
     {
         return readOnlyFooRepository.count();
