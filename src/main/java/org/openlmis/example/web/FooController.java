d577c77f998745341417866c6ca495440138cdf7
Ben Leibert <ben.leibert@villagereach.org>
2016-06-03 15:19:01 -0700

    Give the sample controller an appropriate name.

--- src/main/java/org/openlmis/example/web/FooController.java
+++ src/main/java/org/openlmis/example/web/FooController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class NotificationController
+public class FooController
 {
     @Autowired
     private ReadOnlyFooRepository readOnlyFooRepository;
