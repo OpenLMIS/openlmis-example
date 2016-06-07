46ca1edba68b0ba4615ea380e52ea233c750bf21
Ben Leibert <ben.leibert@villagereach.org>
2016-06-07 10:58:15 -0700

    OLMIS-662: API Data Validation
    Add (recommend) a way of performing bean-level validation. Spring provides two options:
    
    1) Injecting an org.springframework.validation.Validator into a controller and manually invoking it.
    
    2) Using JSR 303 / 349 based bean validation, specified via annotations. Optionally, Hibernate-specific validations can be used instead.
    
    The second approach works seamlessly with Spring Data Rest, and is provided by our current Spring Boot configuration out of the box. It’s thus the approach taken and implicitly recommended by this check-in.
    
    As commented within this check-in’s code, an additional and complimentary type of validation is also recommended for use. Specifically, standard JPA annotations should be specified to enforce constraints that belong at the database level. The archetypical example of this is a unique constraint: it’s implication for a specific bean can’t be determined exclusively by looking at the bean (“bean-level validation”), but rather relies on the overall state of all beans within the database (“database level validation”).

--- src/main/java/org/openlmis/example/domain/Bar.java
+++ src/main/java/org/openlmis/example/domain/Bar.java
@@ -3,12 +3,32 @@ package org.openlmis.example.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
+/*
+    In this example, a Bar isn't simply a Foo's inevitable counterpart. It's a place to get drinks.
+
+    This class partly exists as a way to demonstrate the use of JSR 349's Bean Validation API through
+    the use of annotations. For a list of both standard and hibernate-specific annotations respectively, see:
+
+    http://docs.jboss.org/hibernate/validator/5.0/reference/en-US/html_single/#section-builtin-constraints
+    http://docs.jboss.org/hibernate/validator/5.0/reference/en-US/html_single/#validator-defineconstraints-hv-constraints
+
+    Custom constraints can easily be made, as nicely documented here:
+        http://docs.jboss.org/hibernate/validator/5.0/reference/en-US/html_single/#validator-customconstraints
+    Nevertheless, restraint should be exercised in terms of what kind of validation is performed. For instance, a unique-constraint
+    could be implemented via a custom annotation-based validation. It involves the state of the entire database, though, rather than
+    just that of an indivudual bean. Such validation is thus more approprietly enfirced at the DB level (and potentially via an alternate
+    annotation.
+ */
 @Entity
 @NoArgsConstructor
 public class Bar {
@@ -21,9 +41,13 @@ public class Bar {
 
     @Getter
     @Setter
-    String bar;
+    @NotEmpty
+    String name;
 
     @Getter
     @Setter
-    Long priority;
+    @Min(value=2) //Must at least hold a bartender as well as patron
+    @Max(value=500) //Arbitrary business-rule for the sake of our example
+    Integer capacity;
+
 }
