450d2ec829f1f8053935437684f944bc0b3fafe8
Jakub Kondrat <jkondrat@soldevelo.com>
2016-06-10 12:55:53 +0200

    Making integration tests transactional
    Conflicts:
    	src/integration-test/java/org/openlmis/referencedata/repository/OrderLineRepositoryIT.java

--- src/integration-test/java/org/openlmis/example/repository/NotificationRepositoryIT.java
+++ src/integration-test/java/org/openlmis/example/repository/NotificationRepositoryIT.java
@@ -1,14 +1,8 @@
 package org.openlmis.example.repository;
 
-import org.junit.runner.RunWith;
-import org.openlmis.example.Application;
 import org.openlmis.example.domain.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
 public class NotificationRepositoryIT extends BaseCrudRepositoryIT<Notification> {
 
   @Autowired
