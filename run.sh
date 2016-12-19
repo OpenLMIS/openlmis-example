dec4e4bcf0916f2bb6f2bd9e6e1824fff40a5e93
Jakub Kondrat <jkondrat@soldevelo.com>
2016-12-19 12:43:48 +0100

    OLMIS-1283 Fix extensions not loading

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 node consul/registration.js -c register -f consul/config.json -r consul/api-definition.yaml
-java $JAVA_OPTS -jar /service.jar
+java $JAVA_OPTS -classpath extensions/*:service.jar org.springframework.boot.loader.JarLauncher && java $JAVA_OPTS -jar service.jar
